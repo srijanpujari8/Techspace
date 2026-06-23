1de3007d7454eb72c9c7f18127076705afab5474
TechSpace <srijanpujari8@gmail.com>
2026-06-23 23:03:45 +0530

    production ready firebase + env setup

--- firebase.py
+++ firebase.py
@@ -1,14 +1,22 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
-import json
 
 if not firebase_admin._apps:
 
-    cred_json = os.environ.get("FIREBASE_CREDENTIALS")
+    cred_dict = {
+        "type": os.environ.get("FIREBASE_TYPE"),
+        "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n"),
+        "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+        "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
+    }
 
-    cred = credentials.Certificate(json.loads(cred_json))
-
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
