0b8332473bea8664200049486498ccf350de6925
TechSpace <srijanpujari8@gmail.com>
2026-06-24 02:15:13 +0530

    fix private key

--- firebase.py
+++ firebase.py
@@ -10,7 +10,7 @@ try:
             "type": "service_account",
             "project_id": os.environ["FIREBASE_PROJECT_ID"],
             "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
-            "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
+            "private_key": os.environ["FIREBASE_PRIVATE_KEY"],
             "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
             "client_id": os.environ["FIREBASE_CLIENT_ID"],
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
